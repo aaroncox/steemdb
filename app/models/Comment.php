7f4167ab705117704cbbed811c7bac6966bbad69
Aaron Cox <acox@carbinestudios.com>
2016-09-09 00:24:57 -0700

    rshares api endpoint - reward distribution per day

--- app/models/Comment.php
+++ app/models/Comment.php
@@ -1,6 +1,8 @@
 <?php
 namespace SteemDB\Models;
 
+use MongoDB\BSON\UTCDateTime;
+
 class Comment extends Document
 {
 
@@ -31,5 +33,107 @@ class Comment extends Document
     ));
   }
 
+  public static function rsharesAllocation($dates = []) {
+    ini_set('precision',20);
+    if(!is_array($dates) || !count($dates)) {
+      $dates = [
+        '$gte' => new UTCDateTime(strtotime("-30 days") * 1000),
+        '$lte' => new UTCDateTime(strtotime("midnight") * 1000),
+      ];
+    }
+    return Comment::aggregate([
+      [
+        '$match' => [
+          'created' => $dates
+        ]
+      ],
+      [
+        '$project' => [
+          'author' => 1,
+          'active_votes' => 1,
+          'created' => 1
+        ]
+      ],
+      [
+        '$unwind' => '$active_votes'
+      ],
+      [
+        '$match' => [
+          'active_votes.weight' => ['$ne' => 0]
+        ]
+      ],
+      [
+        '$group' => [
+          '_id' => [
+            'voter' => '$active_votes.voter',
+            'doy' => ['$dayOfYear' => '$created'],
+            'year' => ['$year' => '$created'],
+            'month' => ['$month' => '$created'],
+            'day' => ['$dayOfMonth' => '$created'],
+          ],
+          'votes' => [
+            '$sum' => 1
+          ],
+          'rshares' => [
+            '$sum' => '$active_votes.rshares'
+          ]
+        ]
+      ],
+      [
+        '$sort' => [
+          'rshares' => -1
+        ]
+      ],
+      [
+        '$group' => [
+          '_id' => [
+            'doy' => '$_id.doy',
+            'year' => '$_id.year',
+            'month' => '$_id.month',
+            'day' => '$_id.day',
+          ],
+          'total_voters' => [
+            '$sum' => 1
+          ],
+          'total_rshares' => [
+            '$sum' =>'$rshares'
+          ],
+          'voters' => [
+            '$push' => [
+              'voter' => '$_id.voter',
+              'votes' => '$votes',
+              'rshares' => '$rshares',
+            ]
+          ]
+        ]
+      ],
+      [
+        '$project' => [
+          '_id' => '$_id',
+          'total_voters' => '$total_voters',
+          'total_rshares' => '$total_rshares',
+          'voters' => [
+            '$slice' => [
+              '$voters', 100
+            ]
+          ]
+        ]
+      ],
+      [
+        '$sort' => [
+          '_id.year' => 1,
+          '_id.doy' => 1
+        ]
+      ]
+      // [
+      //   '$limit' => 10
+      // ]
+    ], [
+      'allowDiskUse' => true,
+      'cursor' => [
+        'batchSize' => 0
+      ]
+    ]);
+  }
 }
 
