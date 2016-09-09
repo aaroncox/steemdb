8b5df63925c112fe9eb51109c5fba85833ca9ec0
Aaron Cox <acox@carbinestudios.com>
2016-09-09 00:28:33 -0700

    Removed unused models from namespace

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -1,10 +1,6 @@
 <?php
 namespace SteemDB\Controllers;
 
-use SteemDB\Models\Block;
-use SteemDB\Models\Account;
-use SteemDB\Models\Comment;
-
 class IndexController extends ControllerBase
 {
   public function indexAction()
