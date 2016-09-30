aba669890223753cff872c31eb8e7074756a5df8
Aaron Cox <acox@carbinestudios.com>
2016-09-29 19:00:54 -0700

    Helper function to convert MongoDB\BSON\UTCDateTIme to a string for json_encode

--- app/models/Document.php
+++ app/models/Document.php
@@ -7,6 +7,15 @@ use Phalcon\Mvc\MongoCollection;
 class Document extends MongoCollection
 {
 
+  public function toArray() {
+    foreach($this as $key => $data) {
+      if(get_class($data) == 'MongoDB\BSON\UTCDateTime') {
+        $this->$key = (int) (string) $data;
+      }
+    }
+    return parent::toArray();
+  }
+
   public function export() {
     $toRemove = ['_dependencyInjector','_modelsManager','_source','_operationMade','_connection', '_errorMessages', '_skipped'];
     $data = array_diff_key(get_object_vars($this), array_flip($toRemove));
