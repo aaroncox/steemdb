e0e6a828ac3a5b86a7d36c6dd8eb0fbcb64a7c68
Aaron Cox <acox@carbinestudios.com>
2016-09-29 19:00:35 -0700

    Strip invalid tags when rendering content.
    Tags allowed copied from steemit.com

--- app/helpers/Markdown.php
+++ app/helpers/Markdown.php
@@ -7,6 +7,8 @@ class Markdown
 
   static public function string($string)
   {
+    // Remove any body tags, because it seems to break the purifier
+    $string = strip_tags($string, "<div><iframe><del><a><p><b><q><br><ul><li><ol><img><h1><h2><h3><h4><h5><h6><hr><blockquote><pre><code><em><strong><center><table><thead><tbody><tr><th><td><strike><sup>");
     // Let's turn image URLs into <img> tags
     $regex = "~<img[^>]*>(*SKIP)(*FAIL)|\\[[^\\]]*\\](*SKIP)(*FAIL)|\\([^\\)]*\\)(*SKIP)(*FAIL)|https?://[^/\\s]+/\\S+\\.(?:jpg|png|gif)~i";
     $string = preg_replace($regex, '<img src="${0}">', $string);
