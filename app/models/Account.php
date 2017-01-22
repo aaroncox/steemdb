fa3796ad21c127f73ae2cfb092c3efa4fcd67dd7
Aaron Cox <aaron.cox@greymass.com>
2017-01-22 15:30:34 -0800

    Helper to retrieve the account profile image

--- app/models/Account.php
+++ app/models/Account.php
@@ -4,4 +4,14 @@ namespace SteemDB\Models;
 class Account extends Document
 {
 
+  public function profileImage() {
+    if(property_exists($this, 'json_metadata') && !is_array($this->json_metadata)) {
+        $this->json_metadata = json_decode($this->json_metadata, true);
+    }
+    if(property_exists($this, 'json_metadata') && isset($this->json_metadata['profile']) && isset($this->json_metadata['profile']['profile_image'])) {
+        return $this->json_metadata['profile']['profile_image'];
+    }
+    return null;
+  }
+
 }
